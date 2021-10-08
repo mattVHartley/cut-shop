bd051323b413861fe1b96424fd1914c811a6fd7d
Michael Bishop <production@displaydesigns.net>
2021-10-08 15:40:51 +0100

    Added cascading dropdown menus to the MaterialOrder2 component that render conditionally based on user input. Changed the colorTypesOption array in line with the functionality of the dropdowns. Added calculateArea method

--- src/views/MaterialOrder2.vue
+++ src/views/MaterialOrder2.vue
@@ -1,67 +1,81 @@
 <template>
   <div class="container">
-    <h1 class="title">Material Order 2</h1>
+    <h1 class="title">Material Order 2 {{title}}</h1>
     <div class="form-container">
       <form @submit.prevent="handleSubmit" class="form">
         <div class="section color-type border">
           <div class="form-control">
             <label>Colour/Type</label>
             <select v-model="order.colorType">
-              <option v-for="color in colorTypesOption" v-bind:key="color.id">{{ color }}</option>
+              <option
+                v-for="color in colorTypesOption"
+                v-bind:value="color.id"
+                v-bind:key="color.id"
+              >
+                {{ color.text }}
+              </option>
             </select>
           </div>
           <div class="form-control">
             <label>Thickness</label>
             <select v-model="order.thickness">
-              <option v-for="thickness in thicknessOption" v-bind:key="thickness.id">{{ thickness }}</option>
+              <option
+                v-for="thickness in thicknessOption[order.colorType]"
+                v-bind:value="thickness.id"
+                v-bind:key="thickness.id"
+              >
+                {{ thickness.text }}mm
+              </option>
             </select>
           </div>
         </div>
-        <div class="section width-height border">
+        <div class="section width-length border">
           <div class="form-control">
-            <label>Width</label>
-            <input type="text" v-model="order.width" />
+            <label>Width in mm</label>
+            <input
+              placeholder="mm"
+              class="input"
+              type="number"
+              v-model="order.width"
+            />
           </div>
           <div class="form-control">
-            <label>Height</label>
-            <input type="text" v-model="order.height" />
+            <label>Length in mm</label>
+            <input
+              placeholder="mm"
+              class="input"
+              type="number"
+              v-model="order.length"
+            />
+          </div>
+          <div class="form-control">
+            <label>Area</label>
+            <div class="area">{{ calculateArea() }} SqM</div>
           </div>
           <div class="form-control">
             <label>Total Number of Pcs To Be Cut</label>
-            <input type="text" v-model="order.totalpcs" />
+            <input class="input" type="number" v-model="order.totalpcs" />
           </div>
           <div class="form-control">
-            <button type="submit" class="btn">Add Another Pcs</button>
+            <button type="submit" class="btn">Add To Order</button>
           </div>
         </div>
+
         <div class="section border">
           <div class="display-order">
-              <div class="display-order-title">
-                  <h3 class="display-title">Current Order</h3>
-              </div>
-              <div class="display-order-orders">
-                  <p>Color/Type: {{ order.colorType }}</p>
-                  <p>Thickness: {{ order.thickness }}</p>
-                  <p>Width: {{ order.width }}</p>
-                  <p>Height: {{ order.height }}</p>
-                  <p>Total Number of Pcs To Be Cut: {{ order.totalpcs }}</p>
-              </div>
+            <div class="display-order-title">
+              <h3 class="display-title">{{title}} Orders</h3>
+            </div>
+            <div v-for="order in orders" :key="order" class="orders">
+              <p>{{ order }}</p>
+            </div>
           </div>
         </div>
+        
         <div class="section border">
           <div class="show-cost">
             <h1 class="display-title">Show Cost</h1>
-            <input type="submit" value="Add To Basket" class="btn" />
-          </div>
-        </div>
-        <div class="section border">
-          <div class="display-order">
-              <div class="display-order-title">
-                  <h3 class="display-title">Orders</h3>
-              </div>
-              <div v-for="order in orders" :key="order" class="orders">
-                  <p>{{ order }}</p>
-              </div>
+            <input type="submit" value="Add To Basket" class="btn input" />
           </div>
         </div>
       </form>
@@ -75,34 +89,211 @@ export default {
   data() {
     return {
       order: {
-        colorType: "",
-        thickness: "",
-        width: "",
-        height: "",
-        totalpcs: ""
+        colorType: null,
+        thickness: null,
+        width: null,
+        length: null,
+        sqm: null,
+        totalpcs: null,
       },
-      colorTypesOption: ['red', 'blue', 'black', 'yellow', 'pink'],
-      thicknessOption: ['6mm', '9mm', '12mm', '18mm', '24mm'],
-      orders: []
-    }
+      title: 'Acrylic',
+      colorTypesOption: [
+        {
+          text: "Clear",
+          id: 1,
+        },
+        {
+          text: "Red 4403",
+          id: 2,
+        },
+        {
+          text: "Green 692",
+          id: 3,
+        },
+        {
+          text: "Blue 1743",
+          id: 4,
+        },
+        {
+          text: "White 069",
+          id: 5,
+        },
+        {
+          text: "Black 9030",
+          id: 6,
+        },
+        {
+          text: "LED Opal",
+          id: 7,
+        },
+        {
+          text: "LED Red",
+          id: 8,
+        },
+      ],
+      // colorTypesOption: ['red', 'blue', 'black', 'yellow', 'pink'],
+      thicknessOption: {
+        1: [
+          {
+            text: 2,
+            id: 1,
+          },
+          {
+            text: 3,
+            id: 2,
+          },
+          {
+            text: 5,
+            id: 3,
+          },
+          {
+            text: 6,
+            id: 4,
+          },
+          {
+            text: 8,
+            id: 5,
+          },
+          {
+            text: 10,
+            id: 6,
+          },
+          {
+            text: 15,
+            id: 7,
+          },
+          {
+            text: 20,
+            id: 8,
+          },
+          {
+            text: 30,
+            id: 9,
+          },
+        ],
+        2: [
+          {
+            text: 3,
+            id: 1,
+          },
+          {
+            text: 5,
+            id: 2,
+          },
+        ],
+        3: [
+          {
+            text: 3,
+            id: 1,
+          },
+          {
+            text: 5,
+            id: 2,
+          },
+        ],
+        4: [
+          {
+            text: 3,
+            id: 1,
+          },
+          {
+            text: 5,
+            id: 2,
+          },
+        ],
+        5: [
+          {
+            text: 3,
+            id: 1,
+          },
+          {
+            text: 5,
+            id: 2,
+          },
+          {
+            text: 10,
+            id: 3,
+          },
+          {
+            text: 15,
+            id: 4,
+          },
+          {
+            text: 20,
+            id: 5,
+          },
+        ],
+        6: [
+          {
+            text: 3,
+            id: 1,
+          },
+          {
+            text: 5,
+            id: 2,
+          },
+          {
+            text: 10,
+            id: 3,
+          },
+          {
+            text: 15,
+            id: 4,
+          },
+          {
+            text: 20,
+            id: 5,
+          },
+        ],
+        7: [
+          {
+            text: 10,
+            id: 1,
+          },
+          {
+            text: 30,
+            id: 2,
+          },
+        ],
+        8: [
+          {
+            text: 10,
+            id: 1,
+          },
+          {
+            text: 30,
+            id: 2,
+          },
+        ],
+      },
+      // thicknessOption: ['6mm', '9mm', '12mm', '18mm', '24mm'],
+      orders: [],
+    };
   },
   methods: {
+    calculateArea() {
+      let length = (this.order.length / 1000).toFixed(2);
+      let width = (this.order.width / 1000).toFixed(2);
+      let answer = (length * width).toFixed(2);
+
+      return answer;
+    },
     handleSubmit() {
-      this.orders.push(`Color/Type: ${this.order.colorType}`)
-      this.orders.push(`Thickness: ${this.order.thickness}`)
-      this.orders.push(`Width: ${this.order.width}`)
-      this.orders.push(`Height: ${this.order.height}`)
-      this.orders.push(`Total Number of Pcs To Be Cut: ${this.order.totalpcs}`)
+      this.orders.push(`Color/Type: ${this.order.colorType}`);
+      this.orders.push(`Thickness: ${this.order.thickness}`);
+      this.orders.push(`Area: ${this.calculateArea()} SqM`);
+      this.orders.push(`Total Number of Pcs To Be Cut: ${this.order.totalpcs}`);
 
-      console.log(this.orders)
+      console.log(this.orders);
 
-      this.order.colorType = ""
-      this.order.thickness = ""
-      this.order.width = ""
-      this.order.height = ""
-      this.order.totalpcs = ""
-    }
-  }
+      this.order.colorType = "";
+      this.order.thickness = null;
+      this.order.length = null;
+      this.order.width = null;
+      this.order.sqm = null;
+      this.order.totalpcs = null;
+    },
+  },
 };
 </script>
 
@@ -111,6 +302,13 @@ export default {
   display: flex;
   flex-direction: column;
   /* overflow-y: hidden; */
+}
+
+.area {
+  height: 50px;
+  width: 100px;
+  border-radius: 0.3em;
+  border: 1px solid #30c016;
 }
 
 .orders {
@@ -129,7 +327,7 @@ export default {
   line-height: 1.5;
 }
 
-.orders:nth-child(5n+1) {
+.orders:nth-child(4n + 1) {
   color: red;
   border-bottom: 1px solid red;
   margin-bottom: 1rem;
@@ -151,7 +349,6 @@ export default {
 }
 
 .display-title {
-  
   color: #30c016;
   margin-bottom: 1rem;
   font-size: 2rem;
@@ -170,10 +367,10 @@ export default {
 }
 
 .color-type > .form-control {
-  margin: 1.40rem 0;
+  margin: 1.4rem 0;
 }
 
-.width-height {
+.width-length {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr;
@@ -191,7 +388,17 @@ export default {
   font-size: 1.25rem;
 }
 
-.form-control > input {
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+
+.input {
   font-family: inherit;
   font-size: 1rem;
   margin: 0.35rem;
@@ -201,8 +408,8 @@ export default {
   outline: none;
 }
 
-.form-control:hover > input,
-.form-control:focus > input {
+.input:hover,
+.input:focus {
   box-shadow: 0 0 5px #1ba702;
 }
 
@@ -250,15 +457,15 @@ export default {
 }
 
 .display-order {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .show-cost {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
